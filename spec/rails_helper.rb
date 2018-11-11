194d82ab41a6f5d83371fdd23dc39cfdc622ebea
carloshvcristo <carloshvcristo@hotmail.com>
2018-11-11 19:29:24 -0200

    Adicionadas as rotas

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,4 @@
+# spec/rails_helper.rb
 require 'spec_helper'
 require 'database_cleaner'
 
@@ -5,6 +6,7 @@ require 'database_cleaner'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -29,6 +31,7 @@ RSpec.configure do |config|
   # [...]
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
+  config.include RequestSpecHelper, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
