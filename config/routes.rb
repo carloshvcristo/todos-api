97a2e067dde438a3aec170b14f9dc206f24ffe64
carloshvcristo <carloshvcristo@hotmail.com>
2018-11-11 19:45:05 -0200

    Adicionados endpoints no controller e formatacao de erros e respostas em JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  #In our route definition, we're creating todo resource with a nested items resource. 
+  #This enforces the 1:m (one to many) associations at the routing level. 
   resources :todos do
     resources :items
   end
